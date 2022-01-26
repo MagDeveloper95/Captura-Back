258900d2c7d4e6c6bb790289d4c737bfb6ee9451
raultc95 <raultc95@gmail.com>
2022-01-26 18:25:25 +0100

    Service Foto y Visita

--- src/main/java/com/iesFrancisco/captura/Services/FotoService.java
+++ src/main/java/com/iesFrancisco/captura/Services/FotoService.java
@@ -1,17 +1,66 @@
 package com.iesFrancisco.captura.Services;
 
+import com.iesFrancisco.captura.Exception.RecordNotFoundException;
+import com.iesFrancisco.captura.Model.Foto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.iesFrancisco.captura.Repositories.FotoRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class FotoService {
-	
-	@Autowired // instanciar el repositorio
-	FotoRepository repository;
-	
-	/**
-	 * RELLENAR A PARTIR DE AQUI
-	 */
+
+    @Autowired // instanciar el repositorio
+    FotoRepository repository;
+
+    public List<Foto> getAllFoto() {
+        List<Foto> result = repository.findAll();
+        return result;
+    }
+
+    public Foto getFotoById(Long id) throws RecordNotFoundException {
+        Optional<Foto> result = repository.findById(id);
+        if (result.isPresent()) {
+            return result.get();
+        } else {
+            throw new RecordNotFoundException("Foto no encontrada", id);
+        }
+
+    }
+
+    public Foto createOrUpdateFoto(Foto foto) throws RecordNotFoundException {
+        if (foto.getId() != null && foto.getId() > 0) {
+            Optional<Foto> f = repository.findById(foto.getId());
+            if (f.isPresent()) {
+                Foto newFoto = f.get();
+                newFoto.setId(foto.getId());
+                newFoto.setUrl(foto.getUrl());
+                newFoto.setComentario(foto.getComentario());
+                newFoto.setVisita(foto.getVisita());
+                newFoto = repository.save(newFoto);
+                return newFoto;
+            } else {
+                foto.setId(null);
+                foto = repository.save(foto);
+                return foto;
+            }
+        } else {
+            foto = repository.save(foto);
+            return foto;
+        }
+
+    }
+
+    public void deleteFotoById(Long id) throws RecordNotFoundException {
+        Optional<Foto> foto = repository.findById(id);
+        if (foto.isPresent()) {
+            repository.deleteById(id);
+        } else {
+            throw new RecordNotFoundException("La foto no encontrada", id);
+        }
+    }
+
 }
--- src/main/java/com/iesFrancisco/captura/Services/VisitaService.java
+++ src/main/java/com/iesFrancisco/captura/Services/VisitaService.java
@@ -1,17 +1,64 @@
 package com.iesFrancisco.captura.Services;
 
+import com.iesFrancisco.captura.Exception.RecordNotFoundException;
+import com.iesFrancisco.captura.Model.Visita;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.iesFrancisco.captura.Repositories.VisitaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class VisitaService {
-	
-	@Autowired // instanciar el repositorio
-	VisitaRepository repository;
-	
-	/**
-	 * RELLENAR A PARTIR DE AQUI
-	 */
+
+    @Autowired // instanciar el repositorio
+    VisitaRepository repository;
+
+    public List<Visita> getAllVisita() {
+        List<Visita> result = repository.findAll();
+        return result;
+    }
+
+    public Visita getVisitaById(Long id) throws RecordNotFoundException {
+        Optional<Visita> result = repository.findById(id);
+        if (result.isPresent()) {
+            return result.get();
+        } else {
+            throw new RecordNotFoundException("Visita no encontrada", id);
+        }
+    }
+
+    public Visita createOrUpdateVisita(Visita visita) throws RecordNotFoundException {
+        if (visita.getId() != null && visita.getId() > 0) {
+            Optional<Visita> v = repository.findById(visita.getId());
+            if (v.isPresent()) {
+                Visita newVisita = v.get();
+                newVisita.setId(visita.getId());
+                newVisita.setHeader(visita.getHeader());
+                newVisita.setFecha(visita.getFecha());
+                newVisita.setNota(visita.getNota());
+                newVisita.setFotos(visita.getFotos());
+                newVisita = repository.save(newVisita);
+                return newVisita;
+            } else {
+                visita.setId(null);
+                visita = repository.save(visita);
+                return visita;
+            }
+        } else {
+            visita = repository.save(visita);
+            return visita;
+        }
+    }
+
+    public void deleteVisita(Long id) throws RecordNotFoundException {
+        Optional<Visita> visita = repository.findById(id);
+        if (visita.isPresent()) {
+            repository.deleteById(id);
+        } else {
+            throw new RecordNotFoundException("Visita no encontrada", id);
+        }
+    }
 }
