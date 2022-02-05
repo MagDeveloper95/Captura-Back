ed2192e4da49bfc6c6de4f59c904f7f1f5167668
Miguel Angel <dammiguelangel@gmail.com>
2022-02-05 10:33:15 +0100

    mgm-16 ----> implementacion de log4j2

--- src/main/java/com/iesFrancisco/captura/AppMain.java
+++ src/main/java/com/iesFrancisco/captura/AppMain.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.ComponentScan;
 public class AppMain {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(AppMain.class, args);
 		
 	}
