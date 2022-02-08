461438907b782c19b537a8b672189a4def8d7cd0
Miguel Angel <dammiguelangel@gmail.com>
2022-02-08 19:17:57 +0100

    mgm-20 ---> Servicio Crear Obras y visitas

--- src/main/java/com/iesFrancisco/captura/Services/OneDrive.java
+++ src/main/java/com/iesFrancisco/captura/Services/OneDrive.java
@@ -9,6 +9,11 @@ public class OneDrive {
 	
 	private static GraphServiceClient<Request> graphClient = null;
 	
+	/**
+	 * Metodo que usaremos para crear una carpeta en OneDrive con el nombre que le pasemos de la obra
+	 * el cual usaremos para crear las carpetas de las obras
+	 * @param folderName Nombre de la carpeta que queremos crear en OneDrive
+	 */
 	public static void createObra(String folderName){
 		DriveItem driveItem = new DriveItem();
 		driveItem.name = folderName;
@@ -18,7 +23,12 @@ public class OneDrive {
 		graphClient.users("b9e1d304-a6b1-4d09-aa66-ece2bd6fb7b6").drive().root().children().buildRequest()
 		.post(driveItem);
 	}
-	
+	/**
+	 * Metodo que usaremos para crear una carpeta en OneDrive con el nombre que le pasemos de la visita y el nombre de la obra para 
+	 * guardar la carpeta de la visita dentro de la capeta de la obra correspondeiente
+	 * @param folderName nomre de la carpeta que queremos crear en OneDrive
+	 * @param parentFolderId Id de la carpeta padre de la carpeta que queremos crear
+	 */
 	public static void createVisita(String folderName, String parentFolderId) {
 		DriveItem driveItem = new DriveItem();
 		driveItem.name = folderName;
