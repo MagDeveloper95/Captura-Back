58cec17067485e3bff46533fc12bd66b3cbbc703
Miguel Angel <dammiguelangel@gmail.com>
2022-04-26 10:22:20 +0200

    foto

--- src/main/java/com/iesFrancisco/captura/Repositories/FotoRepository.java
+++ src/main/java/com/iesFrancisco/captura/Repositories/FotoRepository.java
@@ -15,26 +15,10 @@ import com.iesFrancisco.captura.Model.Foto;
 
 @Repository
 public interface FotoRepository extends JpaRepository<Foto,Long> {
-	/**
-	 * Metodo que nos devuelve una lista con los las fotos  
-	 * introduciendo una Visita.
-	 * @param vista por la que queremos las fotos
-	 * @return La lista de las fotos filtrando por visitas
-	 * @throws RecordNotFoundException en caso de no encontrar registros
-	 * @throws ResquestUnauthourized en caso de que no este autorizado
-	 * @throws RecordOK en caso de que todo vaya correctamente.
-	 */
+	
 	@Query(value= "SELECT * FROM foto where foto.id_visita = ?1", nativeQuery=true)
 	List<Foto> getFotosPorVisita(Long id) throws IllegalArgumentException;
-	/**
-	 * Metodo que nos devuelve una lista con los las fotos  
-	 * introduciendo una fecha.
-	 * @param fecha por la que queremos el las fotos
-	 * @return La lista de las fotos filtrando por día
-	 * @throws RecordNotFoundException en caso de no encontrar registros
-	 * @throws ResquestUnauthourized en caso de que no este autorizado
-	 * @throws RecordOK en caso de que todo vaya correctamente.
-	 */
+
 	@Query(value= "SELECT FOT FROM foto FOT where FOT.fecha LIKE %?1", nativeQuery=true)
 	List<Foto> getFotosPorFecha(LocalDate fecha) throws IllegalArgumentException;
 	
--- src/main/java/com/iesFrancisco/captura/Repositories/RegistroRepository.java
+++ src/main/java/com/iesFrancisco/captura/Repositories/RegistroRepository.java
@@ -15,28 +15,11 @@ import com.iesFrancisco.captura.Model.Registro;
 @Repository
 public interface RegistroRepository extends JpaRepository<Registro, Long> {
 
-	/**
-	 * Método que nos devuelve una lista con los registros 
-	 * introduciendo una fecha.
-	 * @param fecha por la que queremos el registro
-	 * @return La lista de registros filtrando por día
-	 * @throws RecordNotFoundException en caso de no encontrar registros
-	 * @throws ResquestUnauthourized en caso de que no este autorizado
-	 * @throws RecordOK en caso de que todo vaya correctamente.
-	 */
+	
 	
 	@Query(value= "SELECT daReg FROM registro daReg where daReg.fecha LIKE %?1", nativeQuery=true)
 	List<Registro> getRegistroPorFecha(LocalDate fecha) throws IllegalArgumentException;
-	/**
-	 * Métodoo que nos devuelve una lista con los registros
-	 * introduciendo un usuario
-	 * @param usuario por el que queremos el registro
-	 * @return La lista de registros filtrando por Usuario
-	 * @throws RecordNotFoundException en caso de no encontrar registros
-	 * @throws ResquestUnauthourized en caso de que no este autorizado
-	 * @throws RecordOK en caso de que todo vaya correctamente
-	 */
-	
+
 	@Query(value= "SELECT daReg FROM registro daReg where daReg.idUser = ?1",nativeQuery=true)
 	List<Registro> getRegistroPorUsuario(Long usuario) throws IllegalArgumentException;
 	
